26ea925b8aadd32cccd1632da3641bca931d1143
georges <georgsilva@gmail.com>
2020-01-10 11:44:00 -0400

    initial

---
+++ src/pages/main.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import api from '../services/api'
+
+import { View, Text, FlatList, TouchableOpacity } from 'react-native'
+
+export default class Main extends Component {
+    static navigationOptions = {
+        title: "JSHunt"
+    }
+
+    state = {
+        docs: [],
+    }
+
+    componentDidMount() {
+        this.loadProducts()
+    }
+
+    loadProducts = async () => {
+        const response = await api.get("/products")
+
+        const { docs } = response.data
+
+        this.setState({ docs })
+    }
+
+    renderItem = ({ item }) => (
+        <View>
+            <Text>{item.title}</Text>
+            <Text>{item.description}</Text>
+            <TouchableOpacity onPress={() => {}}></TouchableOpacity>
+        </View>
+    )
+
+    render() {
+        return (
+            <View>
+                <FlatList 
+                    data={this.state.docs} 
+                    keyExtractor={item => item._id} 
+                    renderItem={this.renderItem} />
+            </View>
+        )
+    }
+}
